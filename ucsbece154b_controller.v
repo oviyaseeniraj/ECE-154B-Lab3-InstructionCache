ecbb61c53f8267f8eebf21e6ec08a7a7156e999d
Justin Wu <justin_wu@ucsb.edu>
2025-05-13 11:34:22 -0700

    remove ready_f dependency for firedonce

--- ucsbece154b_controller.v
+++ ucsbece154b_controller.v
@@ -216,11 +216,11 @@ reg firedOnce;
 always @(posedge clk) begin
   if (reset)
     firedOnce <= 0;
-  else if (Ready_F)
+  else
     firedOnce <= 1;
 end
 
-assign StallF_o = lwStall || ~Ready_F;
+assign StallF_o = lwStall || (firedOnce && ~Ready_F);
 
  assign StallD_o = lwStall || ~Ready_F;
  assign FlushD_o = MisspredictE_i;
