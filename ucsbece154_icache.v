767f70505b9a18559a6ae7114e9d6825f91e21f7
Justin Wu <justin_wu@ucsb.edu>
2025-05-13 14:58:28 -0700

    try blocking more

--- ucsbece154_icache.v
+++ ucsbece154_icache.v
@@ -100,12 +100,12 @@ always @ (posedge Clk) begin
         if (hit_this_cycle) begin
             $display("hit at time %0t, read_address=%h, set_index=%0b, hit_way=%0b, word_offset=%0b", 
                 $time, ReadAddress, set_index, hit_way, latchedReadAddress[OFFSET-1:WORD_OFFSET]);
-            hit_latched <= 1;
-            latched_hit_way <= hit_way;
-            latchedReadAddress <= ReadAddress; // NEW
-            latched_set_index <= set_index;
+            hit_latched = 1;
+            latched_hit_way = hit_way;
+            latchedReadAddress = ReadAddress; // NEW
+            latched_set_index = set_index;
         end else begin
-            hit_latched <= 0;
+            hit_latched = 0;
         end
 
         if (hit_latched) begin
